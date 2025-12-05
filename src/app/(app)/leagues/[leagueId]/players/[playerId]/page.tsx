2a1943afc5b4a14262f7616570d939c390bd055a
m k <markus.koenigreich@gmail.com>
2025-12-05 13:40:52 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/(app)/leagues/[leagueId]/players/[playerId]/page.tsx
+++ src/app/(app)/leagues/[leagueId]/players/[playerId]/page.tsx
@@ -1,7 +1,7 @@
 
 
 import { getPlayerStats } from "@/lib/data";
-import { notFound, useParams } from "next/navigation";
+import { notFound } from "next/navigation";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { ArrowUp, ArrowDown, User as UserIcon } from "lucide-react";
